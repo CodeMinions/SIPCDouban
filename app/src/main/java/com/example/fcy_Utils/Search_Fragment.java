2e13ee2e8050bda3e62c549763e8a2e8da74d668
fcy <1575047842@qq.com>
2020-07-10 20:30:28 +0800

    找片fragment完善

---
+++ app/src/main/java/com/example/fcy_Utils/Search_Fragment.java
@@ -0,0 +1,126 @@
+package com.example.fcy_Utils;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.sipcdouban.R;
+import com.example.sipcdouban.databinding.FragmentSearchBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Search_Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Search_Fragment extends Fragment implements View.OnClickListener {
+    private int current_type = -1;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Search_Fragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Search_Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Search_Fragment newInstance(String param1, String param2) {
+        Search_Fragment fragment = new Search_Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: ");
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    private static final String TAG = "Search_Fragment";
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        FragmentSearchBinding binding = DataBindingUtil.inflate(inflater,R.layout.fragment_search_,container,false);
+        binding.setMContext(this);
+        binding.setLifecycleOwner(getActivity());
+        Log.d(TAG, "onCreateView: ");
+        // Inflate the layout for this fragment
+        return binding.getRoot();
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d(TAG, "onViewCreated: ");
+        changeLabel(0);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.b1:
+                break;
+            case R.id.b2:
+                break;
+            case R.id.b3:
+                break;
+            case R.id.b4:
+                break;
+            default:
+        }
+    }
+
+    private final int[][] ids_1 = new int[][]{{R.drawable.pic_search, R.drawable.rank_list_pic, R.drawable.dou_ban_cai_pic, R.drawable.movies_list_pic}
+            , {R.drawable.pic_search, R.drawable.rank_list_pic, R.drawable.dou_ban_cai_pic,R.drawable.schedule_pic}};
+    private final int[][] ids_2 = new int[][]{{R.string.search_movie, R.string.rank_list, R.string.dou_ban_cai, R.string.movie_list}
+            , {R.string.search_tv,R.string.rank_list,R.string.dou_ban_cai,R.string.schedule}};
+
+    public void changeLabel(int x) {
+        if (x == current_type) return;
+        else current_type = (current_type + 1) % 2;
+        int[] ids = new int[]{R.id.b1, R.id.b2, R.id.b3, R.id.b4};
+        for (int i = 0; i < 4; i++) {
+            View view = getView().findViewById(ids[i]);
+            ImageView imageView = view.findViewById(R.id.icon);
+            TextView title = view.findViewById(R.id.title);
+            imageView.setImageResource(ids_1[current_type][i]);
+            title.setText(getResources().getString(ids_2[current_type][i]));
+        }
+    }
+
+}
--- app/src/main/java/com/example/sipcdouban/MainActivity.java
+++ app/src/main/java/com/example/sipcdouban/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.sipcdouban;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.fcy_Utils.Search_Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView mBottomNavigationView;
+    private Fragment[] fragments = new Fragment[]{new Search_Fragment()};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBottomNavigationView = findViewById(R.id.my_bottom_view);
+        mBottomNavigationView.setSelectedItemId(R.id.search_movie);
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_fragment,fragments[0]).commit();
+        mBottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.search_movie:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.content_fragment, fragments[0]).commit();
+                        break;
+                    case R.id.hot_show:
+                        // 更改到热搜fragment
+                        break;
+                    default:
+                }
+            }
+        });
     }
 }
